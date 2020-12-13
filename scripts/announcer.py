fb08e6a0b33301e798e2888794f03d567188b7cc
aharonmaghen <aharon.maghen27@students.kbcc.cuny.edu>
2020-12-13 17:40:20 +0200

    Update announcer

--- scripts/announcer.py
+++ scripts/announcer.py
@@ -2,5 +2,5 @@ import pyttsx3
 
 def announce(stockName, stockPrice):
 	engine = pyttsx3.init()
-	engine.say(stockName + " is currently at " + stockPrice)
+	engine.say(stockName + " is currently at " + stockPrice + " dollars")
 	engine.runAndWait()
